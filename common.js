1c3bf83ee878ffee7dff65a1b99e03045ca3b298
smgoogle <51619588+smgoogle@users.noreply.github.com>
2019-07-27 16:14:29 +0800

    Add files via upload

--- common.js
+++ common.js
@@ -16,6 +16,7 @@ function analyseData(data){
 	$.cookie('trying', baseInfos[10].split("\t")[0]);
 	$.cookie('tryingRegStatus', baseInfos[10].split("\t")[1]);
 	$.cookie('tryingCkInStatus', baseInfos[10].split("\t")[2]);
+	$.cookie('tryingRegNum','100');
 	
 	$.cookie('cash', baseInfos[11].split("\t")[0]);
 	$.cookie('withdrawed', baseInfos[11].split("\t")[1]);
